49f70bee00b25d79956d94f67820fe31afeaf851
John Knotts <jhnnyk@gmail.com>
2024-06-20 10:32:29 -0600

    creating the signup hook

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -5,5 +5,30 @@ export const useSignup = () => {
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
 
-  
+  const signup = async (email, password, displayName) => {
+    setError(null)
+    setIsPending(true)
+
+    try {
+      // signup user
+      const res = await projectAuth.createUserWithEmailAndPassword(email, password)
+      console.log(res.user)
+
+      if (!res) {
+        throw new Error('could not complete signup')
+      }
+
+      // add displayName to user
+      await res.user.updateProfile({ displayName })
+
+      setIsPending(false)
+      setError(null)
+    } catch (err) {
+      console.log(err.message)
+      setError(err.message)
+      setIsPending(false)
+    }
+  }
+
+  return { error, isPending, signup }
 }
