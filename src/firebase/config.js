60be276c62b2c111b3ab0c1fc23aaa174158c7d8
John Knotts <jhnnyk@gmail.com>
2024-06-19 20:48:55 -0600

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/compat/app"
+import 'firebase/compat/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB01fKBII-de4FemhaDWmbJt1XMG2pTtdc",
+  authDomain: "netninja-mymoney-e7765.firebaseapp.com",
+  projectId: "netninja-mymoney-e7765",
+  storageBucket: "netninja-mymoney-e7765.appspot.com",
+  messagingSenderId: "473075927548",
+  appId: "1:473075927548:web:0752f2081e1edfc6930a79"
+}
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init service
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
